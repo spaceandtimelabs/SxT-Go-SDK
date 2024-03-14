6aec5f8fd2a493de9f20c44516403c78ed5c8c69
Lester Kim <lester.c.kim@gmail.com>
2024-03-14 12:59:18 -0400

    Clean up storage files (#8)

--- storage/aws.go
+++ storage/aws.go
@@ -11,50 +11,45 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
-type AwsSessionStruct struct{
-	AccessToken string `json:"accessToken"`
-	RefreshToken string `json:"refreshToken"`
-	PrivateKey ed25519.PrivateKey `json:"privateKey"`
-	PublicKey ed25519.PublicKey `json:"publicKey"`
+type AwsSessionStruct struct {
+	AccessToken  string             `json:"accessToken"`
+	RefreshToken string             `json:"refreshToken"`
+	PrivateKey   ed25519.PrivateKey `json:"privateKey"`
+	PublicKey    ed25519.PublicKey  `json:"publicKey"`
 }
 
 // Write session data to aws secrets manager: accessToken, refreshToken, publicKey, privateKey
 // Filename to be stored as `userId` (provided by SxT)
-func AwsWriteSession(userId, accessToken, refreshToken string, privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey)( status bool){
-
+func AwsWriteSession(userId, accessToken, refreshToken string, privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey) (status bool) {
 	if accessToken == "" || refreshToken == "" {
 		return false
 	}
-	
- 	sessionStruct := AwsSessionStruct{
-		AccessToken: accessToken,
+
+	sessionStruct := AwsSessionStruct{
+		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
-		PublicKey: publicKey,
-		PrivateKey: privateKey,
+		PublicKey:    publicKey,
+		PrivateKey:   privateKey,
 	}
 
-	sessionData, e := json.Marshal(sessionStruct)
+	sessionData, err := json.Marshal(sessionStruct)
 
-	if e != nil {
+	if err != nil {
 		return false
 	} else {
-		
-		sess, _ := awsAuthenticate()
-		
+		secretsManagerClient := getSecretsManagerClient()
 		input := &secretsmanager.CreateSecretInput{
-			Description:        aws.String("Credentials for "+ userId),
-			Name:               aws.String(userId), // In live, you can put the sxt username (userId) here
-			SecretString:       aws.String(string(sessionData)),
+			Description:  aws.String("Credentials for " + userId),
+			Name:         aws.String(userId), // In live, you can put the sxt username (userId) here
+			SecretString: aws.String(string(sessionData)),
 		}
 
-		svc := secretsmanager.NewFromConfig(sess)
-		result, err := svc.CreateSecret(context.TODO(), input)
-
+		secret, err := secretsManagerClient.CreateSecret(context.TODO(), input)
 		if err != nil {
 			return false
 		}
 
-		fmt.Println("WRITE Session", result)
+		fmt.Println("WRITE Session", secret)
 	}
 
 	return true
@@ -62,74 +57,61 @@ func AwsWriteSession(userId, accessToken, refreshToken string, privateKey ed2551
 
 // Read session data from aws secrets manager: accessToken, refreshToken, publicKey, privateKey
 // Filename to be retrieved as `userId` (provided by SxT)
-func AwsReadSession(userId string)(sessionStruct AwsSessionStruct, status bool){
-
-	sess, _ := awsAuthenticate()
-
+func AwsReadSession(userId string) (sessionStruct AwsSessionStruct, status bool) {
+	secretsManagerClient := getSecretsManagerClient()
 	input := &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String(userId), // In live, you can put the sxt username (userId) here
 	}
 
-	svc := secretsmanager.NewFromConfig(sess)
-	result, err := svc.GetSecretValue(context.TODO(), input)
-	if err != nil {
-		return AwsSessionStruct{}, false
-	}
-
-	// fmt.Println("READ", *result.SecretString)
-    e := json.Unmarshal([]byte(*result.SecretString), &sessionStruct)
-	if e != nil {
+	secret, err := secretsManagerClient.GetSecretValue(context.TODO(), input)
+	if err != nil || json.Unmarshal([]byte(*secret.SecretString), &sessionStruct) != nil {
 		return AwsSessionStruct{}, false
 	}
 
 	return sessionStruct, true
 }
 
-
 // Update session data to aws secrets manager: accessToken, refreshToken, publicKey, privateKey
 // Filename to be updated as `userId` (provided by SxT)
-func AwsUpdateSession(userId, accessToken, refreshToken string, privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey)( status bool){
-
+func AwsUpdateSession(userId, accessToken, refreshToken string, privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey) (status bool) {
 	if accessToken == "" || refreshToken == "" {
 		return false
 	}
-	
- 	sessionStruct := AwsSessionStruct{
-		AccessToken: accessToken,
+
+	sessionStruct := AwsSessionStruct{
+		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
-		PublicKey: publicKey,
-		PrivateKey: privateKey,
+		PublicKey:    publicKey,
+		PrivateKey:   privateKey,
 	}
 
-	sessionData, e := json.Marshal(sessionStruct)
-
-	if e != nil {
+	sessionData, err := json.Marshal(sessionStruct)
+	if err != nil {
 		return false
 	} else {
-		
-		sess, _ := awsAuthenticate()
-		
+		secretsManagerClient := getSecretsManagerClient()
 		input := &secretsmanager.PutSecretValueInput{
-			SecretId:           aws.String(userId), // In live, you can put the sxt username (userId) here
-			SecretString:       aws.String(string(sessionData)),
+			SecretId:     aws.String(userId), // In live, you can put the sxt username (userId) here
+			SecretString: aws.String(string(sessionData)),
 		}
 
-		svc := secretsmanager.NewFromConfig(sess)
-		result, err := svc.PutSecretValue(context.TODO(), input)
-
+		secret, err := secretsManagerClient.PutSecretValue(context.TODO(), input)
 		if err != nil {
 			return false
 		}
 
-		fmt.Println("UPDATE Session", result)
+		fmt.Println("UPDATE Session", secret)
 	}
 
 	return true
 }
 
-// aws authenticate
-func awsAuthenticate()(cfg aws.Config, err error){
+func getSecretsManagerClient() *secretsmanager.Client {
+	session, _ := awsAuthenticate()
 
-	cfg , err = config.LoadDefaultConfig(context.TODO());
-	return cfg, err
+	return secretsmanager.NewFromConfig(session)
+}
+
+func awsAuthenticate() (cfg aws.Config, err error) {
+	return config.LoadDefaultConfig(context.TODO())
 }
--- storage/file.go
+++ storage/file.go
@@ -3,49 +3,50 @@ package storage
 import (
 	"crypto/ed25519"
 	"encoding/json"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
-type FileSessionStruct struct{
-	AccessToken string `json:"accessToken"`
-	RefreshToken string `json:"refreshToken"`
-	PrivateKey ed25519.PrivateKey `json:"privateKey"`
-	PublicKey ed25519.PublicKey `json:"publicKey"`
+type FileSessionStruct struct {
+	AccessToken  string             `json:"accessToken"`
+	RefreshToken string             `json:"refreshToken"`
+	PrivateKey   ed25519.PrivateKey `json:"privateKey"`
+	PublicKey    ed25519.PublicKey  `json:"publicKey"`
 }
 
 // Write session data to file: accessToken, refreshToken, publicKey, privateKey
 // Note: This is for demo purposes only. To make a secure session, save the credentials in db or some other secure source
-func FileWriteSession(userId, accessToken, refreshToken string, privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey)(status bool){
-
+func FileWriteSession(userId, accessToken, refreshToken string, privateKey ed25519.PrivateKey, publicKey ed25519.PublicKey) (status bool) {
 	if accessToken == "" || refreshToken == "" {
 		return false
 	}
 
- 	sessionStruct := FileSessionStruct{
-		AccessToken: accessToken,
+	sessionStruct := FileSessionStruct{
+		AccessToken:  accessToken,
 		RefreshToken: refreshToken,
-		PublicKey: publicKey,
-		PrivateKey: privateKey,
+		PublicKey:    publicKey,
+		PrivateKey:   privateKey,
 	}
 
-	sessionData, e := json.Marshal(sessionStruct)
+	sessionData, err := json.Marshal(sessionStruct)
 
-	if e != nil {
-		log.Println(e.Error())
+	if err != nil {
+		log.Println(err.Error())
 	} else {
-		
+		filepath := "./tmp/" + userId + ".txt"
+		everyoneCanReadWriteAndExecute := fs.FileMode(0777)
+		file, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY, everyoneCanReadWriteAndExecute)
 
-		f, errCreate := os.OpenFile("./tmp/" + userId + ".txt", os.O_CREATE|os.O_WRONLY, 0777)
-		if errCreate != nil {
-			log.Println(errCreate.Error())
+		if err != nil {
+			log.Println(err.Error())
 			return false
 		}
-	
-		defer f.Close()
-	
-		_, errWrite := f.Write(sessionData)
+
+		defer file.Close()
+
+		_, errWrite := file.Write(sessionData)
 		if errWrite != nil {
 			log.Println(errWrite.Error())
 			return false
@@ -57,18 +58,10 @@ func FileWriteSession(userId, accessToken, refreshToken string, privateKey ed255
 
 // Read session data from file: accessToken, refreshToken, publicKey, privateKey
 // Note: This is for demo purposes only. To make a secure session, save the credentials in db or some other secure source
-func FileReadSession(userId string)(sessionStruct FileSessionStruct, status bool){
-
+func FileReadSession(userId string) (sessionStruct FileSessionStruct, status bool) {
 	content, err := ioutil.ReadFile("./tmp/" + userId + ".txt")
 
-     if err != nil {
-        //   log.Fatal(err)
-		  return FileSessionStruct{}, false
-     }
-
-    e := json.Unmarshal(content, &sessionStruct)
-	if e != nil {
-		// log.Println(e.Error())
+	if err != nil || json.Unmarshal(content, &sessionStruct) != nil {
 		return FileSessionStruct{}, false
 	}
 
